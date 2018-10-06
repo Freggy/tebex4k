edf7d3a61635f8192b45d15715282a11cee05521
freggy <riegeryannic@gmail.com>
2018-10-06 12:49:04 +0200

    Initial commit

---
+++ src/main/kotlin/de/freggy/tebex4k/Tebex.kt
@@ -0,0 +1,20 @@
+package de.freggy.tebex4k
+
+/**
+ * Created by Yannic Rieger on 06.10.2018.
+ * <p>
+ * @author Yannic Rieger
+ */
+class Tebex {
+
+    companion object Factory {
+
+        fun async(): TebexHttpClient {
+
+        }
+
+        fun sync(): TebexHttpClient {
+
+        }
+    }
+}
---
+++ src/main/kotlin/de/freggy/tebex4k/TebexHttpClient.kt
@@ -0,0 +1,10 @@
+package de.freggy.tebex4k
+
+/**
+ * Created by Yannic Rieger on 06.10.2018.
+ * <p>
+ * @author Yannic Rieger
+ */
+interface TebexHttpClient {
+
+}
